f84ccf479a7f4ee5555af3a25cdc040fb21c2008
pranay101 <pranayprajapati101@gmail.com>
2021-05-30 16:24:24 +0530

    implemented playlist and add music

--- function.py
+++ function.py
@@ -1,6 +1,39 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+# create the root window
+root = tk.Tk()
+root.title('Tkinter File Dialog')
+root.resizable(False, False)
+root.geometry('300x150')
 
 
-#functions required for the media player to run
+def select_files():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+
+    filenames = fd.askopenfilenames(
+        title='Open files',
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected Files',
+        message=filenames
+    )
 
 
-#Quit function for exiting the program
+# open button
+open_button = ttk.Button(
+    root,
+    text='Open Files',
+    command=select_files
+)
+
+open_button.pack(expand=True)
+
+root.mainloop()
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from tkinter import *
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+import pygame
 #import function
 
 
-#core functions
-def _quit():
-    window.quit()
-    window.destroy()
-    exit()
+
 
 
 #declare the window variable
@@ -14,12 +13,41 @@ window =Tk()
 
 #give a suitable name to the window
 window.title("Mediaplayer")
+window.iconbitmap(r"icon.ico")
 
 #Set it's height and width
-window.configure(width = 700, height=500)
+window.geometry("400x300")
 
 #give a background color to the window
 window.configure(bg='#1a1b1c')
+
+# core functions 
+# All core function starts with underscore("_")
+def _quit():
+    window.quit()
+    window.destroy()
+    exit()
+
+def _openfile():
+    Song_location = fd.askopenfilename(title="Choose A Song", filetypes=(("mp3 Files", "*.mp3"), ))
+    song_name= Song_location.split('/')[-1].split('.')[0]    
+    
+    playlist.insert(END,song_name)
+
+    
+pygame.mixer.init()# initialise the pygame
+
+#initilize frame
+master_frame = Frame(window)
+master_frame.pack(pady=20)
+
+playlist = Listbox(master=master_frame, bg="#1a1b1c", fg="white", width=60, selectbackground="#d6d6d6", selectforeground="black")
+playlist.grid(row=0, column=0)
+
+name="prana prajapat"
+playlist.insert(END,name)
+
+
 
 
 
@@ -31,7 +59,7 @@ window.config(menu=menuBar)
 
 #File menu
 filemenu = Menu(menuBar, tearoff=0,)
-filemenu.add_command(label="Open")
+filemenu.add_command(label="Add Music", command=_openfile)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=_quit)
 menuBar.add_cascade(label="File", menu = filemenu)
